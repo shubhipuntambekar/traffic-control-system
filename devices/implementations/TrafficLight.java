136541bb2374c54dabacec0259f1b31330ba57c0
shubhi <shubhipun@gmail.com>
2023-12-01 19:30:23 +0530

    Added classes and interfaces

---
+++ devices/implementations/TrafficLight.java
@@ -0,0 +1,16 @@
+package devices.implementations;
+
+import commons.DeviceState;
+import devices.Device;
+
+public class TrafficLight implements Device {
+    @Override
+    public DeviceState getState() {
+        return null;
+    }
+
+    @Override
+    public void sendData() {
+
+    }
+}
